9592786535314229e03c7bc2693a34cc657ed369
Thomas Mathews <thomas@monstercat.com>
2019-01-18 10:19:11 -0800

    WIP

--- src/js/account.js
+++ src/js/account.js
@@ -53,8 +53,8 @@ function submitSaveAccount (e, el) {
   const wasLegacy = isLegacyLocation()
 
   submitForm(e, {
-    url: endpoint + '/self',
-    method: 'PATCH',
+    url: endpoint2 + '/self',
+    method: 'PUT',
     transformData: transformSubmittedAccountData,
     validate: validateAccountData,
     success: function (result, data) {
@@ -72,11 +72,14 @@ function submitSaveAccount (e, el) {
 
 function submitSaveAccountSettings (e, el) {
   submitForm(e, {
-    url: endpoint + '/self/settings',
-    method: 'PATCH',
+    url: endpoint2 + '/self',
+    method: 'PUT',
+    transformData: (data) => {
+      return {settings: data}
+    },
     success: function (result, data) {
       toasty(strings.settingsUpdated)
-      session.settings = data
+      session.settings = data.settings
     }
   })
 }
@@ -89,23 +92,22 @@ function toggleStreamerMode(e, el) {
   hideTracks.checked = el.checked
   blockTracks.checked = el.checked
   request({
-    url: `${endpoint}/self/settings`,
-    data: {
+    url: `${endpoint2}/self`,
+    method: 'PUT',
+    data: {settings: {
       hideNonLicensableTracks: hideTracks.checked,
       blockNonLicensableTracks: blockTracks.checked,
-    },
+    }},
     cors: true,
-    method: 'PATCH',
   }, (err, body, xhr) => {
     if (err) {
       toasty(err)
       return
     }
-    session.settings = body
-    if (el.checked){
+    session.settings = body.settings
+    if (el.checked) {
       toasty("Streamer mode activated. Happy content creating!")
-    }
-    else {
+    } else {
       toasty(Error("Streamer mode deactivated. Non-licensable tracks used will receive claims."))
     }
   })
@@ -117,8 +119,11 @@ function submitSaveRedditUsername (e, el) {
 
   submitForm(e, {
     method: 'PUT',
-    url: endpoint + '/self/update-reddit',
-    success: function (args) {
+    url: endpoint2 + '/self/update-reddit',
+    error: (err) => {
+      alert(err.message)
+    },
+    success: (args) => {
       if (unsetUsername) {
         toasty('Flair cleared')
       }
@@ -131,10 +136,9 @@ function submitSaveRedditUsername (e, el) {
 
 function submitJoinDiscord (e, el) {
   const responseEl = findNode('[role=join-discord] [role=response]')
-
   submitForm(e, {
     method: "POST",
-    url: endpoint + "/self/discord/join",
+    url: endpoint2 + "/self/discord/join",
     started: function () {
       betterRender('discord-response', responseEl, {loading: true})
     },
@@ -142,7 +146,6 @@ function submitJoinDiscord (e, el) {
       if (!data.discordId) {
         errs.push('Please provide a Discord User ID')
       }
-
       return errs
     },
     successMsg: 'Gold channel joined!'
@@ -160,16 +163,6 @@ function submitVerifyInvite (e, el) {
   })
 }
 
-function saveShopEmail (e, el) {
-  var data = getTargetDataSet(el, true, true)
-  if (!data) return
-  update('self', null, data, function (err, obj) {
-    if (err) return window.alert(err.message)
-    toasty(strings.shopEmailUpdated)
-    session.user.shopEmail = data.shopEmail
-  })
-}
-
 function transformTwoFactorFormData (data) {
   data.number = String(data.number)
   return data
@@ -177,8 +170,8 @@ function transformTwoFactorFormData (data) {
 
 function enableTwoFactor (e, el) {
   submitForm(e, {
-    method: 'PUT',
-    url: endpoint + '/self/two-factor',
+    method: 'POST',
+    url: endpoint2 + '/self/two-factor',
     transformData: transformTwoFactorFormData,
     validate: function (data, errs) {
       if (!data.countryCode) {
@@ -187,8 +180,10 @@ function enableTwoFactor (e, el) {
       if (!data.number) {
         errs.push('Number is required')
       }
-
       return errs
+    },
+    error: (err) => {
+      alert(err.message)
     },
     success: function () {
       window.location.hash = '#two-factor'
@@ -201,8 +196,11 @@ function enableTwoFactor (e, el) {
 function confirmTwoFactor (e, el) {
   submitForm(e, {
     transformData: transformTwoFactorFormData,
-    url: endpoint + '/self/two-factor/confirm',
-    method: 'PUT',
+    url: endpoint2 + '/self/two-factor/confirm',
+    method: 'POST',
+    error: (err) => {
+      alert(err.message)
+    },
     success: function () {
       reloadPage()
       window.location.hash = '#two-factor'
@@ -213,8 +211,8 @@ function confirmTwoFactor (e, el) {
 
 function disableTwoFactor (e, el) {
   requestJSON({
-    url: endpoint + '/self/two-factor/disable',
-    method: 'PUT',
+    url: endpoint2 + '/self/two-factor/disable',
+    method: 'POST',
     withCredentials: true
   }, (err, obj, xhr) => {
     if (terror(err)) {
@@ -348,7 +346,7 @@ function processAccountGoldPage (args) {
     return
   }
   requestJSON({
-    url: endpoint + '/self',
+    url: endpoint2 + '/self',
     withCredentials: true
   }, (err, selfResult) => {
     if (err) {
@@ -440,7 +438,7 @@ function processAccountSettings (args) {
         return opt
       })
 
-      scope.settings = result
+      scope.settings = result.settings
       const { settings } = scope
 
       scope.isStreamerMode = settings.blockNonLicensableTracks &&
